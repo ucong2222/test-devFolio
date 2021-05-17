08024af6bea340c73b27c7afa71ff259e1112e49
ucong2222 <dbrudrjf21@gmail.com>
2021-05-17 22:34:35 +0900

    첫 커밋

---
+++ src/main/java/com/sbs/devFolio/TestDevFolioApplication.java
@@ -0,0 +1,13 @@
+package com.sbs.devFolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDevFolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDevFolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbs/devFolio/TestDevFolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.devFolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDevFolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
