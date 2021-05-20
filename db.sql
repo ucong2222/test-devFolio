10a9757b039c27336e8387a4fb4901cc6203c69f
ucong2222 <dbrudrjf21@gmail.com>
2021-05-20 13:23:48 +0900

    외부 로그인을 위해 회원 테이블에 authKey 칼럼 추가

--- db.sql
+++ db.sql
@@ -28,3 +28,14 @@ SET regDate = NOW(),
     nickname = "테스트1",
     email = "dbrudrjf21@gmail.com",
     cellphoneNo = "01012341234";
+
+# authKey 칼럼을 추가
+ALTER TABLE `member` ADD COLUMN authKey CHAR(80) NOT NULL AFTER loginPw;
+
+# authKey 칼럼에 유니크 인덱스 추가
+ALTER TABLE `test_devFolio`.`member` ADD UNIQUE INDEX (`authKey`);
+
+# 기존 회원의 authKey 데이터 채우기
+UPDATE `member`
+SET authKey = CONCAT("authKey1__", UUID(), "__", RAND())
+WHERE authKey = '';
