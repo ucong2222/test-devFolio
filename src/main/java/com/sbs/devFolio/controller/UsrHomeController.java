1244626b5d7a939dbe1993b888eceb0030765d28
ucong2222 <dbrudrjf21@gmail.com>
2021-06-19 02:23:07 +0900

    usr/home/main 접속시 usr/pages/portFolio 페이지 가져오기

--- src/main/java/com/sbs/devFolio/controller/UsrHomeController.java
+++ src/main/java/com/sbs/devFolio/controller/UsrHomeController.java
@@ -1,14 +1,19 @@
 package com.sbs.devFolio.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class UsrHomeController {
+public class UsrHomeController extends BaseController {
+	@Autowired
+	private UsrArticleController usrArticleController;
 
 	@RequestMapping("/usr/home/main")
-	public String showMain() {
-		return "/usr/pages/portFolio";
+	public String showMain(HttpServletRequest req) {
+		return usrArticleController.showPortFolio(1, req);
 	}
-	
+
 }
