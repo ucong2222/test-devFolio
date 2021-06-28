f6bb49901728f9669244bdaa94c6737d72eda752
ucong2222 <dbrudrjf21@gmail.com>
2021-06-28 22:15:04 +0900

    genFile 테이블 추가

--- db.sql
+++ db.sql
@@ -157,3 +157,26 @@ CREATE TABLE `like`(
 # 좋아요 인덱스 추가
 # 인덱스 : 테이블에 저장된 데이터를 빠르게 조회하기 위한 데이터베이스 객체
 ALTER TABLE `like` ADD INDEX(relTypeCode, relId, memberId);
+
+# 파일 테이블 추가
+# 파일규격코드 하는 이유 : jpg, jpeg 등 정리해서 들어가는 부분, 자세한 규격이 들어가는 부분
+CREATE TABLE genFile (
+    id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, # 번호
+    regDate DATETIME DEFAULT NULL, # 작성날짜
+    updateDate DATETIME DEFAULT NULL, # 갱신날짜
+    delDate DATETIME DEFAULT NULL, # 삭제날짜
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0, # 삭제상태(0:미삭제, 1:삭제)
+    relTypeCode CHAR(50) NOT NULL, # 관련 데이터 타입 ( article, memeber . . .)
+    relId INT(10) UNSIGNED NOT NULL, # 관련 데이터 번호
+    originFileName VARCHAR(100) NOT NULL, # 업로드 당시의 파일이름
+    fileExt CHAR(10) NOT NULL, # 확장자
+    typeCode CHAR(20) NOT NULL, # 종류코드(common)
+    type2Code CHAR(20) NOT NULL, # 종류2코드 (attachment)
+    fileSize INT(10) UNSIGNED NOT NULL, # 파일의 사이즈
+    fileExtTypeCode CHAR(10) NOT NULL, # 파일규격코드 (img, video)
+    fileExtType2Code CHAR(10) NOT NULL, # 파일규격2코드 (jpg, mp4)
+    fileNo SMALLINT(2) UNSIGNED NOT NULL, # 파일번호 (1)
+    fileDir CHAR(20) NOT NULL, # 파일이 저장되는 폴더명
+    PRIMARY KEY (id),
+    KEY relId (relId, relTypeCode, typeCode, type2Code, fileNo)
+);
